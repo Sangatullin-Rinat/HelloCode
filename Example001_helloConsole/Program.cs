62b4481678b8f04933b53dca50195458afdd245f
Rinat <rinat.salamov@bk.ru>
2022-07-01 14:45:46 +0300

    main

--- Example001_helloConsole/Program.cs
+++ Example001_helloConsole/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("2 Hello, World! ");
+﻿int a = 3; 
+int b = 7;
+
+int max = a;
+
+if (a > max)  max = a;
+if (b > max)  max = b;
+
+Console.Write ("max = ");
+Console.WriteLine (max);("2 Hello, World! ");
+
+
