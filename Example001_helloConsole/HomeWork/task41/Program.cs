eefba00718e7d0c5c704d1c237d51c5271ed8d5d
Rinat <rinat.salamov@bk.ru>
2022-07-18 19:26:22 +0300

    task41 remaster

--- Example001_helloConsole/HomeWork/task41/Program.cs
+++ Example001_helloConsole/HomeWork/task41/Program.cs
@@ -1,32 +1,30 @@
 // Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-int ReadNaturalInt(string promtingMessageForUser)
+
+Console.WriteLine("Вводитe сколько угодно цифр через пробел");
+Console.Write("\n");
+long countOfPositiveNumbers = 0;
+string input = Console.ReadLine();
+
+
+for (int i = 0; i < input.Length; i++) 
 {
-    int result = -1;
-    while (result <= 0)
+    if (input[i] == 48 && i < input.Length - 1)
     {
-        Console.Write(promtingMessageForUser);
-        result = Convert.ToInt32(Console.ReadLine());
-        if (result <= 0)
-        {
-            Console.WriteLine("введено ненатуральное число");
-        }
+        while (input[i] == 48 && i < input.Length - 1) i++;
     }
-    return result;
+    if (input[i] >= 49 && input[i] <= 57) countOfPositiveNumbers++;
+    if (input[i] == 45 && i < input.Length - 1) i++;
+    while ((input[i] >= 48 && input[i] <= 57)
+            && i < input.Length - 1) i++;
 }
+Console.Write("\n");
+ Console.WriteLine("Количество чисел больше нуля - {0}.", countOfPositiveNumbers);
+ Console.Write("\n");
+ Console.WriteLine("Получилась переменная типа String?  - Да");
+ Console.Write("\n");
+ Console.WriteLine("Поискали в интернете метод для парсинга строки в массив чисел через сеператор(разделитель) - Нет  ");
+ Console.Write("\n");
+ Console.WriteLine("Прошлись по данному массиву и посчитали  колличество положительных чисел?  - Да, несколько раз");
+ Console.Write("\n");
 
-int countOfNumbers = ReadNaturalInt("Сколько чисел будем вводить? ");
-int countOfPositiveNumbers = 0;
-double currentNumber = -1;
-
-for (int i = 1; i <= countOfNumbers; i++)
-{
-    Console.Write("Введите " + i + " число - ");
-    currentNumber = Convert.ToDouble(Console.ReadLine());
-    if (currentNumber > 0) 
-    {
-        countOfPositiveNumbers++;
-    }
-}
-
-Console.WriteLine("Количество положительных чисел среди введенных - " + countOfPositiveNumbers);
