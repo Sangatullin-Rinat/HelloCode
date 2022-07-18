bbda559b44757e1f7b06cb97444c5dcf18c0a714
Rinat <rinat.salamov@bk.ru>
2022-07-18 19:40:09 +0300

    task50 remaster

--- Example001_helloConsole/HomeWork/task50/Program.cs
+++ Example001_helloConsole/HomeWork/task50/Program.cs
@@ -1,26 +1,26 @@
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 
-Console.WriteLine("Enter M and N");
-int M = Convert.ToInt32(Console.ReadLine());
-int N = Convert.ToInt32(Console.ReadLine());   
+Console.WriteLine("Enter X and Y");
+int X = Convert.ToInt32(Console.ReadLine());
+int Y = Convert.ToInt32(Console.ReadLine());   
 
 
-int[,] array = new int [M,N]; 
+int[,] array = new int [X,Y]; 
 
 Random rnd = new Random(); 
 
-for (int i = 0; i < M; i++) {
-    for (int j = 0; j < N; j++) {
+for (int i = 0; i < X; i++) {
+    for (int j = 0; j < Y; j++) {
          array [i,j] = rnd.Next(-10, 10);
 
         Console.Write(array[i,j]+ " ");
      }
     Console.WriteLine();
 }
-for (int j = 0; j < N; j++) {
+for (int j = 0; j < Y; j++) {
     double sum = 0;
-    for (int i = 0; i < M; i++) {
+    for (int i = 0; i < X; i++) {
         sum += array[i, j];
     }
-     Console.WriteLine("sum is {0}", sum / M);
+     Console.WriteLine("sum is {0}", sum / X);
 }
